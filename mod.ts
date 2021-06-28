6375806da4d90e9d535efbcea8dc8a869b0627d8
aiotter <git@aiotter.com>
2021-06-28 16:37:19 +0900

    Execute Autoformat by deno

--- mod.ts
+++ mod.ts
@@ -1,15 +1,17 @@
 import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.12-alpha/deno-dom-wasm.ts";
 import { startBot } from "https://deno.land/x/discordeno@11.2.0/mod.ts";
 import * as helpers from "https://deno.land/x/discordeno@11.2.0/src/helpers/mod.ts";
-import { DiscordenoMessage } from "https://deno.land/x/discordeno@11.2.0/src/structures/message.ts"
+import { DiscordenoMessage } from "https://deno.land/x/discordeno@11.2.0/src/structures/message.ts";
 import { Message } from "https://deno.land/x/discordeno@11.2.0/src/types/messages/message.ts";
 import { Embed } from "https://deno.land/x/discordeno@11.2.0/src/types/mod.ts";
 import { camelize } from "https://deno.land/x/discordeno@11.2.0/src/util/utils.ts";
 
-
-const amazonRegex = new RegExp("https?://.*?amazon\\.co\\.jp.*/(gp(/product)?|dp|ASIN)/([^/?]{10,})\\S*", "g");
-const webhookName = "Amazon-URL-Shortener"
-const footer = "Powered by Amazon URL Shortener (@aiotter)"
+const amazonRegex = new RegExp(
+  "https?://.*?amazon\\.co\\.jp.*/(gp(/product)?|dp|ASIN)/([^/?]{10,})\\S*",
+  "g",
+);
+const webhookName = "Amazon-URL-Shortener";
+const footer = "Powered by Amazon URL Shortener (@aiotter)";
 let processingMessageIds: bigint[] = [];
 
 interface AmazonData {
@@ -23,7 +25,7 @@ function isEmbedAlreadyProcessed(embed: Embed) {
 }
 
 async function updateEmbeds(embeds: Embed[]) {
-  const modifiedEmbeds = []
+  const modifiedEmbeds = [];
   for (const embed of embeds) {
     if (isEmbedAlreadyProcessed(embed) || !embed.url?.match(amazonRegex)) {
       modifiedEmbeds.push(embed);
@@ -33,7 +35,7 @@ async function updateEmbeds(embeds: Embed[]) {
     const amazon = await fetchAmazonData(embed.url as string);
     modifiedEmbeds.push(appendAmazonEmbed(embed, amazon));
   }
-  return modifiedEmbeds
+  return modifiedEmbeds;
 }
 
 async function ensureWebhook(channelId: bigint) {
@@ -41,28 +43,33 @@ async function ensureWebhook(channelId: bigint) {
   for (const webhook of webhooks.values()) {
     if (webhook.token) return webhook;
   }
-  return await helpers.createWebhook(channelId, {name: webhookName});
+  return await helpers.createWebhook(channelId, { name: webhookName });
 }
 
 function appendAmazonEmbed(embed: Embed, amazon: AmazonData) {
-  embed.thumbnail = {url: amazon.imageUrl};
+  embed.thumbnail = { url: amazon.imageUrl };
   embed.fields = [
-    {name: "価格", value: amazon.price ?? "？？？"},
-    {name: "評価", value: amazon.rating ?? "？？？"},
-  ]
-  embed.footer = {text: footer};
-  return embed
+    { name: "価格", value: amazon.price ?? "？？？" },
+    { name: "評価", value: amazon.rating ?? "？？？" },
+  ];
+  embed.footer = { text: footer };
+  return embed;
 }
 
 async function fetchAmazonData(url: string) {
   const res = await fetch(url);
   const html = await res.text();
-  const document = new DOMParser().parseFromString(html, 'text/html');
+  const document = new DOMParser().parseFromString(html, "text/html");
   return {
-    price: document?.querySelector("#price,#newBuyBoxPrice,#priceblock_ourprice,#kindle-price,#price_inside_buybox,.slot-price>.a-color-price")?.textContent.replace(/^\s*(.*)\s*$/, '$1'),
-    imageUrl: document?.querySelector("#landingImage,#imgBlkFront,#ebooksImgBlkFront")?.getAttribute('src') ?? undefined,
-    rating: document?.querySelector('span[data-hook="rating-out-of-text"]')?.textContent,
-  }
+    price: document?.querySelector(
+      "#price,#newBuyBoxPrice,#priceblock_ourprice,#kindle-price,#price_inside_buybox,.slot-price>.a-color-price",
+    )?.textContent.replace(/^\s*(.*)\s*$/, "$1"),
+    imageUrl:
+      document?.querySelector("#landingImage,#imgBlkFront,#ebooksImgBlkFront")
+        ?.getAttribute("src") ?? undefined,
+    rating: document?.querySelector('span[data-hook="rating-out-of-text"]')
+      ?.textContent,
+  };
 }
 
 startBot({
@@ -83,7 +90,7 @@ startBot({
       const webhook = await ensureWebhook(message.channelId);
       if (!webhook) {
         await message.send("Webhook を作成できません！");
-        setTimeout(()=>message.delete(), 5);
+        setTimeout(() => message.delete(), 5);
         return;
       }
 
@@ -91,22 +98,31 @@ startBot({
         const updatedEmbeds = await updateEmbeds(message.embeds);
         const webhook = await helpers.getWebhook(message.webhookId);
         // if (JSON.stringify(updatedEmbeds) === JSON.stringify(message.embeds)) return;
-        helpers.editWebhookMessage(BigInt(webhook.id), webhook.token as string, {
-          messageId: message.id,
-          embeds: updatedEmbeds,
-        })
+        helpers.editWebhookMessage(
+          BigInt(webhook.id),
+          webhook.token as string,
+          {
+            messageId: message.id,
+            embeds: updatedEmbeds,
+          },
+        );
         return;
       }
 
-      processingMessageIds = processingMessageIds.filter(id => id !== message.id);
+      processingMessageIds = processingMessageIds.filter((id) =>
+        id !== message.id
+      );
       // processingMessageIds = processingMessageIds.slice(0,50);
 
       // オリジナルメッセージを削除し，短縮リンクに置き換えて投稿する
       if (!message.webhookId) {
         const { username, discriminator, id, avatar } = message.toJSON().author;
         helpers.sendWebhook(BigInt(webhook.id), webhook.token as string, {
-          content: message.content.replaceAll(amazonRegex, (_0, _1, _2, asin) =>
-            `https://www.amazon.co.jp/gp/product/${asin}/`),
+          content: message.content.replaceAll(
+            amazonRegex,
+            (_0, _1, _2, asin) =>
+              `https://www.amazon.co.jp/gp/product/${asin}/`,
+          ),
           username: `${username}#${discriminator}`,
           avatarUrl: `https://cdn.discordapp.com/avatars/${id}/${avatar}.png`,
         })
@@ -132,22 +148,34 @@ startBot({
         // すでに messageCreate で削除済みのメッセージに関するイベントが発生した場合に対応
         let message: DiscordenoMessage;
         try {
-          message = await helpers.getMessage(BigInt(partialMessage.channelId), BigInt(partialMessage.id));
-        } catch (error) { console.log(error); return; }
+          message = await helpers.getMessage(
+            BigInt(partialMessage.channelId),
+            BigInt(partialMessage.id),
+          );
+        } catch (error) {
+          console.log(error);
+          return;
+        }
 
         if (!message.webhookId || message.embeds.length === 0) return;
 
         const updatedEmbeds = await updateEmbeds(message.embeds);
         const webhook = await helpers.getWebhook(message.webhookId);
         // if (JSON.stringify(updatedEmbeds) === JSON.stringify(message.embeds)) return;
-        helpers.editWebhookMessage(BigInt(webhook.id), webhook.token as string, {
-          messageId: message.id,
-          embeds: updatedEmbeds,
-        })
+        helpers.editWebhookMessage(
+          BigInt(webhook.id),
+          webhook.token as string,
+          {
+            messageId: message.id,
+            embeds: updatedEmbeds,
+          },
+        );
 
-        processingMessageIds = processingMessageIds.filter(id => id !== message.id);
+        processingMessageIds = processingMessageIds.filter((id) =>
+          id !== message.id
+        );
         // processingMessageIds = processingMessageIds.slice(0,50);
       }
-    }
+    },
   },
 });
