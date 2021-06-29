6868080494eb9cd19c923c5fd4cef9ba5a490e77
aiotter <git@aiotter.com>
2021-06-29 23:01:14 +0900

    Update denon automation

--- scripts.config.ts
+++ scripts.config.ts
@@ -4,9 +4,9 @@ import { config as dotenv } from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
 const config: DenonConfig = {
   scripts: {
     start: {
-      cmd: "deno run -A mod.ts",
+      cmd: "deno run mod.ts",
       desc: "Make the bot online",
-      allow: ["env", "write"],
+      allow: ["env", "write", "read", "net"],
       unstable: true,
       env: dotenv(),
     },
